261052b75285291d26dff541cc46e9036a5bc486
Jump <teboul.clement@gmail.com>
2020-06-09 18:08:35 +0200

    Add transition

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app">
+  <div id='app'>
     <div class='layout'>
-      <div class="aside">
+      <div class='aside'>
         <LeftAside />
       </div>
       <main>
-        <router-view/>
+        <transition
+          :name='transitionName'
+          mode='out-in'
+        >
+          <router-view/>
+        </transition>
       </main>
     </div>
     <ParticleBackground />
@@ -21,6 +26,20 @@
     components: {
       LeftAside,
       ParticleBackground
+    },
+    data() {
+      return {
+        transitionName: ''
+      }
+    },
+    created() {
+      this.$router.beforeEach((to, from, next) => {
+        const toDepth = to.path.split('/').length;
+        const fromDepth = from.path.split('/').length;
+        this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left';
+
+        next();
+      });
     }
   }
 </script>
@@ -49,5 +68,27 @@
     justify-content: center;
     align-items: center;
     margin-left: 16rem;
+  }
+
+  .slide-left-enter-active,
+  .slide-left-leave-active,
+  .slide-right-enter-active,
+  .slide-right-leave-active {
+    transition-duration: 0.25s;
+    transition-property: opacity, transform;
+    transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+    overflow: hidden;
+  }
+
+  .slide-left-enter,
+  .slide-right-leave-active {
+    opacity: 0;
+    transform: translate(2rem, 0);
+  }
+
+  .slide-left-leave-active,
+  .slide-right-enter {
+    opacity: 0;
+    transform: translate(-2rem, 0);
   }
 </style>
