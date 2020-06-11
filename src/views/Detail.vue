e52351b8606bc3caa12ae98f395a362e7bb11ae5
Jump <teboul.clement@gmail.com>
2020-06-11 16:35:32 +0200

    Update style

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <div class="entete">
+    <div class='header-section'>
       <h2>Quelques détails sur moi</h2>
-      <p><span class="soustitre">Nous avons peut être des points communs</span></p>
+      <p><span class='subtitle'>Nous avons peut être des points communs</span></p>
     </div>
-    <p class='p'>
-      <span class="bold">Actuellement</span>, je recherche une nouvelle mission après deux années de travail au sein de la startup Yuco. Partis d'un monolothite PhP, j'ai participé à la refonte de la plateform dont la mission principale était de séparer le front du back. On a ainsi choisit de developper deux applications distincts: une api Php sous le framework API platform et une application front utilisant React. Au cours de ces deux années, j'ai eu le plaisir de me former sur de nouvelle technologies comme React ou Docker mais aussi me perfectionner sur PhP<br><br>
-      <span class="bold">Ce que je préfère dans le développement</span>, c'est la liberté d'action dans notre travail&nbsp;. Partir de rien, ou presque, et petit à petit construire une application. Il n'y a pas qu'une solution pour parvenir à notre objectif mais une multitude de possibilitées. Il est toujours intéressant de discuter de notre solution avec quelqu'un, de comparer notre raisonnement avec le sien, en poser le pour et le contre et pourquoi pas revenir sur ce qu'on a fait.<br>
-      <span class="bold">La joie, le sourire, l'émerveillement des gens</span> après avoir essayé notre application ou notre jeu est quelque chose qui me motive à continuer de travailler.<br><br>
-      <span class="bold">Durant 3 années d'étude à l'Ecole Multimedia</span>, j'ai été formé aux technologies du web: HTML5, CSS3, PHP et JS. J'y ai appris à utiliser des frameworks comme Bootstrap, Phaser, Angular, Vuejs et Silex ainsi que des librairies comme JQuery.<br><br>
+    <p>
+      <span class='bold'>Actuellement</span>, je recherche une nouvelle mission après deux années de travail au sein de la startup Yuco. Partis d'un monolothite PhP, j'ai participé à la refonte de la plateform dont la mission principale était de séparer le front du back. On a ainsi choisit de developper deux applications distincts: une api Php sous le framework API platform et une application front utilisant React. Au cours de ces deux années, j'ai eu le plaisir de me former sur de nouvelle technologies comme React ou Docker mais aussi me perfectionner sur PhP<br><br>
+      <span class='bold'>Ce que je préfère dans le développement</span>, c'est la liberté d'action dans notre travail&nbsp;. Partir de rien, ou presque, et petit à petit construire une application. Il n'y a pas qu'une solution pour parvenir à notre objectif mais une multitude de possibilitées. Il est toujours intéressant de discuter de notre solution avec quelqu'un, de comparer notre raisonnement avec le sien, en poser le pour et le contre et pourquoi pas revenir sur ce qu'on a fait.<br>
+      <span class='bold'>La joie, le sourire, l'émerveillement des gens</span> après avoir essayé notre application ou notre jeu est quelque chose qui me motive à continuer de travailler.<br><br>
+      <span class='bold'>Durant 3 années d'étude à l'Ecole Multimedia</span>, j'ai été formé aux technologies du web: HTML5, CSS3, PHP et JS. J'y ai appris à utiliser des frameworks comme Bootstrap, Phaser, Angular, Vuejs et Silex ainsi que des librairies comme JQuery.<br><br>
       Mais je ne suis pas que développeur. J'aime jouer de la guitare, apprendre de nouveau morceau et de nouvelles techniques. Les avancées scientifiques, les phénomènes spatiaux comme les trous noirs ou encore les comics sont autant de chose qui me passionne de découvrir.<br><br>
       N'hésitez pas à me <router-link to='contact' tag='a' exact>contacter</router-link> si vous souhaitez avoir des informations complémentaires.
     </p>
@@ -16,33 +16,13 @@
 </template>
 
 <style scoped>
-.entete {
-    line-height: 3.2rem;
-    margin-bottom: 5%;
-}
-
-h1, h2 {
-    font-size: 3.2rem;
-    text-transform: uppercase;
-    color: #F54F29;
-}
-
-.entete .soustitre {
-    font-weight: 300;
-    font-size: 3rem;
-    text-transform: lowercase;
-    color: rgba(255,255,255,0.6);
-}
-
-.bold {
-    font-weight: 800;
-}
-
-.p {
-  line-height: 2.4rem;
-}
-
-a {
-  display: inline-block;
-}
+  p {
+    line-height: 2.4rem;
+  }
+  @media (max-width: 1450px) {
+      p {
+          font-size: 1.4rem;
+          line-height: 2rem;
+      }    
+  }
 </style>
