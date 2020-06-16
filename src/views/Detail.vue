0bab90c0aa7a47f01b3768a2e2b64eee3672f49f
Jump <teboul.clement@gmail.com>
2020-06-16 15:15:53 +0200

    Fix typo

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,14 +2,16 @@
   <div>
     <div class='header-section'>
       <h2>Quelques détails sur moi</h2>
-      <p><span class='subtitle'>Nous avons peut être des points communs</span></p>
+      <p><span class='subtitle'>Nous avons peut-être des points communs</span></p>
     </div>
     <p>
-      <span class='bold'>Actuellement</span>, je recherche une nouvelle mission après deux années de travail au sein de la startup Yuco. Partis d'un monolothite PhP, j'ai participé à la refonte de la plateform dont la mission principale était de séparer le front du back. On a ainsi choisit de developper deux applications distincts: une api Php sous le framework API platform et une application front utilisant React. Au cours de ces deux années, j'ai eu le plaisir de me former sur de nouvelle technologies comme React ou Docker mais aussi me perfectionner sur PhP<br><br>
-      <span class='bold'>Ce que je préfère dans le développement</span>, c'est la liberté d'action dans notre travail&nbsp;. Partir de rien, ou presque, et petit à petit construire une application. Il n'y a pas qu'une solution pour parvenir à notre objectif mais une multitude de possibilitées. Il est toujours intéressant de discuter de notre solution avec quelqu'un, de comparer notre raisonnement avec le sien, en poser le pour et le contre et pourquoi pas revenir sur ce qu'on a fait.<br>
-      <span class='bold'>La joie, le sourire, l'émerveillement des gens</span> après avoir essayé notre application ou notre jeu est quelque chose qui me motive à continuer de travailler.<br><br>
-      <span class='bold'>Durant 3 années d'étude à l'Ecole Multimedia</span>, j'ai été formé aux technologies du web: HTML5, CSS3, PHP et JS. J'y ai appris à utiliser des frameworks comme Bootstrap, Phaser, Angular, Vuejs et Silex ainsi que des librairies comme JQuery.<br><br>
-      Mais je ne suis pas que développeur. J'aime jouer de la guitare, apprendre de nouveau morceau et de nouvelles techniques. Les avancées scientifiques, les phénomènes spatiaux comme les trous noirs ou encore les comics sont autant de chose qui me passionne de découvrir.<br><br>
+      <span class='bold'>J'ai commencé à m'intéresser au développement dès la 5èm</span>. Sur le site du zéro, j'ai pu créer mes premiers programmes en C, puis en C++ en 4ème et enfin en Java durant la 3ème. Étant jeune à cette époque, je n'ai en général suivi que la moitié des cours. Mais cela me suffisait amplement pour m'amuser à développer divers jeux textuels.<br />
+      <span class='bold'>Arriver en 2nd,</span> je me suis amusé à programmer divers jeux sur la calculatrice Ti-nspire dans un LUA modifié par le fabricant.<br />
+      <span class='bold'>En terminale,</span> je me suis orienté vers l'option ISN ( informatique et science du numérique ). J'y ai développé un Megaman-like dans le cadre de l'épreuve finale du bac.<br /><br />
+      <span class='bold'>Durant les 3 années suivantes, j'ai étudié à l'École Multimédia</span>. J'ai été formé aux technologies du web: HTML5, CSS3, PHP et JS. J'y ai appris à utiliser des frameworks comme Bootstrap, Phaser, Vuejs et Silex ainsi que des librairies comme JQuery.<br /><br />
+      <span class='bold'>Ce que je préfère dans le développement</span>, c'est la liberté d'action dans notre travail&nbsp;. Partir de rien, ou presque, et petit à petit construire une application. Il n'y a pas qu'une solution pour parvenir à notre objectif mais une multitude de possibilitées. Il est toujours intéressant de discuter de notre solution avec quelqu'un, de comparer notre raisonnement avec le sien, en poser le pour et le contre et pourquoi pas revenir sur ce qu'on a fait.<br />
+      <span class='bold'>La joie, le sourire, l'émerveillement des gens</span> après avoir essayé notre application ou notre jeu est quelque chose qui me motive à continuer de travailler.<br /><br />
+      <span class='bold'>Mais je ne suis pas que développeur</span>. Guitariste à mes heures perdues, j'aime apprendre de nouveau morceau et de nouvelles techniques. Je suis grand fan de lego technics et nouvellement de Gunpla. Les phénomènes paranormaux et astronomiques, les faits scientifiques, les comics ou encore les mangas sont autant de choses qui me passionnent.<br /><br />
       N'hésitez pas à me <router-link to='contact' tag='a' exact>contacter</router-link> si vous souhaitez avoir des informations complémentaires.
     </p>
   </div>
@@ -21,8 +23,8 @@
   }
   @media (max-width: 1450px) {
       p {
-          font-size: 1.4rem;
-          line-height: 2rem;
+          font-size: 1.3rem;
+          line-height: 1.9rem;
       }    
   }
 </style>
