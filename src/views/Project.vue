445bcdea95da3e7b6fdea3c4c280777324ee5ed6
Jump <teboul.clement@gmail.com>
2020-06-16 17:05:32 +0200

    Refaco project view

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -7,58 +7,16 @@
   :navigationNextLabel="'<p class=navigationCursor>&#62;</p>'"
   paginationPosition='bottom'
   >
-    <Slide>
-      <div class="image" :style="{backgroundImage: momoImg}"></div>
+    <Slide v-for="project in getProjects" :key="project.name">
+      <div class="image" :style="{backgroundImage: project.picture}"></div>
       <div class="infoProjet">
-        <p class="titre">Momo le noyau</p>
-        <p class="resume">Developpé pendant la Double Game Week de l'Ecole Multimedia en groupe de 4</p>
+        <p class="titre">{{ project.name}}</p>
+        <p class="resume" v-html="project.resume" />
         <div class="slide-footer">
-          <p class="tags-container"><span class="tags couleur-js">JavaScript </span><span class="tags couleur-default">Phaser </span><span class="tags couleur-html">HTML5 </span><span class="tags couleur-css">CSS3 </span></p>
-          <a :href="momoProject" class="button">voir</a>
-        </div>
-      </div>
-    </Slide>
-    <Slide>
-      <div class="image" :style="{backgroundImage: alzerImg}"></div>
-      <div class="infoProjet">
-        <p class="titre">Alzer Chronicle</p>
-        <p class="resume">Prototype d'un system de combat en tour par tour developpé en tant que projet de fin d'année de classe préparatoire</p>
-        <div class="slide-footer">
-          <p class="tags-container"><span class="tags couleur-js">JavaScript </span><span class="tags couleur-default">Phaser </span></p>
-          <a :href="alzerProject" class="button">voir</a>
-        </div>
-      </div>
-    </Slide>
-    <Slide>
-      <div class="image" :style="{backgroundImage: hcrImg}"></div>
-      <div class="infoProjet">
-        <p class="titre">Hyper Cosmic Racer</p>
-        <p class="resume">Il s'agit d'un projet commun a toute la classe de 1er année, soit 12 personnnes, ou le but était de developper un browser game en 2 semaines. J'ai principalement travailler sur le developpement des modules, du vaisseau spacial et sur la creation et la mise en place de la base de donnée</p>
-        <div class="slide-footer">
-          <p class="tags-container"><span class="tags couleur-js">JavaScript </span><span class="tags couleur-default">PHP </span><span class="tags couleur-html">HTML5 </span><span class="tags couleur-default">MySQL </span></p>
-          <a :href="hcrProject" class="button">voir</a>
-        </div>
-      </div>
-    </Slide>
-    <Slide>
-      <div class="image" :style="{backgroundImage: leapzikImg}"></div>
-      <div class="infoProjet">
-        <p class="titre">LeapZik</p>
-        <p class="resume">1 semaine de developpement, un binome, un LeapMotion et une idée. Quoi de mieux pour créer LeapZik ?<br>Merci à <span class="colleague-name">Victor MOUTTON</span> d'avoir mené ce projet avec moi</p>
-        <div class="slide-footer">
-          <p class="tags-container"><span class="tags couleur-js">JavaScript </span><span class="tags couleur-default">LeapMotion </span><span class="tags couleur-html">HTML5 </span><span class="tags couleur-css">CSS3 </span></p>
-          <a :href="leapZikProject" class="button">voir</a>
-        </div>
-      </div>
-    </Slide>
-    <Slide>
-      <div class="image" :style="{backgroundImage: mrAntiniverseImg}"></div>
-      <div class="infoProjet">
-        <p class="titre">Mr AntiUniverse</p>
-        <p class="resume">Developpé en 1 semaine avec <span class="colleague-name">Clément DUSSOL</span> pour apprendre a utiliser Three.js. Le but de la semaine était de developpé un shoot'em up</p>
-        <div class="slide-footer">
-          <p class="tags-container"><span class="tags couleur-js">JavaScript </span><span class="tags couleur-default">Three.js </span></p>
-          <a :href="mrAntiniverseProject" class="button">voir</a>
+          <p class="tags-container">
+            <span v-for="tag in project.tags" :key="tag" :class="'tags ' + getColorTagClass(tag)">{{ tag }}</span>
+          </p>
+          <a :href="project.link" class="button" target="_blank">voir</a>
         </div>
       </div>
     </Slide>
@@ -66,33 +24,46 @@
 </template>
 
 <script>
-import momoImg from '../assets/momo_le_noyau.png';
-import alzerImg from '../assets/alzer.png';
-import hcrImg from '../assets/hcr.png';
-import leapzikImg from '../assets/leapzik.png';
-import mrAntiniverseImg from '../assets/mr-antiniverse.png';
-import { Carousel, Slide } from 'vue-carousel';
+  import { Carousel, Slide } from 'vue-carousel';
+  import projectsData from '../projects.json';
 
-export default {
-  components: {
-    Carousel,
-    Slide
-  },
-  data() {
-    return {
-      momoImg: 'url(' + momoImg + ')',
-      momoProject: process.env.BASE_URL + 'projets/momo_le_noyau/index.html',
-      alzerImg: 'url(' + alzerImg + ')',
-      alzerProject: process.env.BASE_URL + 'projets/alzer/index.html',
-      hcrImg: 'url(' + hcrImg + ')',
-      hcrProject: 'http://hcr.alwaysdata.net/index.php/login',
-      leapzikImg: 'url(' + leapzikImg + ')',
-      leapZikProject: process.env.BASE_URL + 'projets/leapzik.mp4', 
-      mrAntiniverseImg: 'url(' + mrAntiniverseImg + ')',
-      mrAntiniverseProject: process.env.BASE_URL + 'projets/Mr-AntiUniverse/index.html',
-    }
+  const TAG_NAME_TO_COLOR = {
+    'javascript': 'javascript',
+    'html5': 'html',
+    'css3': 'css'
   }
-};
+
+  export default {
+    components: {
+      Carousel,
+      Slide
+    },
+
+    computed: {
+      getProjects() {
+
+        return projectsData.reverse().map( project => {
+          const link = (project.link.startsWith('http') ? project.link : process.env.BASE_URL + project.link);
+          const picture = 'url(' + require('@/' + project.picture) + ')';
+          return {
+            ...project,
+            picture,
+            link
+          }
+        });
+      }
+    },
+
+    methods: {
+      getColorTag( tag ) {
+        return TAG_NAME_TO_COLOR[tag.toLowerCase()] || 'default';
+      },
+
+      getColorTagClass( tag ) {
+        return 'couleur-' + this.getColorTag(tag);
+      }
+    }
+  };
 </script>
 
 <style>
@@ -144,7 +115,7 @@ export default {
       color: white;
   }
 
-  .couleur-js {
+  .couleur-javascript {
       background-color: #f0c025;
   }
 
